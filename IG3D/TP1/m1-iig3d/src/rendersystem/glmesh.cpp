e646fe6255019f8b25311ac5e7b763b418c75b0b
Wedge <wedg@hotmail.fr>
2014-01-31 10:16:21 +0100

    commit du vendredi

--- IG3D/TP1/m1-iig3d/src/rendersystem/glmesh.cpp
+++ IG3D/TP1/m1-iig3d/src/rendersystem/glmesh.cpp
@@ -25,22 +25,59 @@ namespace rendersystem {
 
         // Début du code à écrire
         // 1 - Générer un identifiant pour un VertexArrayObject et le stocker dans l'attribut mVertexArrayObject
-        // 2 - Générer deux identifiants pour deux VertexBufferObject (un pour les sommets, l'autre pour les faces) et les stocker dans l'attribut mVertexBufferObjects
-        // 3 - Activez le VertexArrayObject
+		glAssert( glGenVertexArrays(1, &vertexArrayObject_) );
+
+		
+		// 2 - Générer deux identifiants pour deux VertexBufferObject (un pour les sommets, l'autre pour les faces) et les stocker dans l'attribut mVertexBufferObjects
+        glAssert( glGenBuffers(2, VertexBufferObjects_));
+		
+		// 3 - Activez le VertexArrayObject
+		glAssert( glBindVertexArray(VertexArrayObject_));
+		
         // 4 - Activez le VertexBufferObject contenant les sommets (GL_ARRAY_BUFFER)
+		glAssert( glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjects_[VBO_VERTICES]) );
+		
         // 5 - Remplir le VertexBufferObject contenant les sommets
-        // 6 - Décrire l'organisation mémoire (glVertexAttribPointer) permettant à OpenGL d'accéder aux informations
+		glAssert( glBufferData(GL_ARRAY_BUFFER,nbVertices_ * sizeof(Vertex), &(vertices[0]),GL_STATIC_DRAW));
+
+		
+		// 6 - Décrire l'organisation mémoire (glVertexAttribPointer) permettant à OpenGL d'accéder aux informations
         // de position (index 0), de normale (index 1) et de coordonnées de texture (index 2) pour chaque sommet.
-        // position
+		GLuint stride = sizeof(Vertex);
+		GLboolean normalized= GL_FALSE;
+		GLenum type = GL_FLOAT;
+							   
+		// position
+							   GLuint index=0;
+							   GLint size=3;
+							   GLvoid *pointer = BUFFER_OFFSET(0)
+							   glAssert( glVertexAttribPointer(index,size,type,normalized,stride,pointer) );
+							   glAssert( glEnableVertexAttribArray(index) );
 
         // normal
+							   
+							   index=1;
+							   size = 3;
+							   pointer=BUFFER_OFFSET(sizeof(glm::vec3));
+							   glAssert(glVertexAttribPointer(index,size,type,normalized, stride, pointer) );
+							   glAssert( glEnableVertexAttribArray(index) );
 
         // texcoord
+							   index=2;
+							   size=2;
+							   pointer=BUFFER_OFFSET(2*sizeof(glm::vec3));
+							   glAssert( glVertexAttribPointer(index,size,type,normalized, stride, pointer) );
+							   glAssert( glEnableVertexAttribArray(index) );
+							   
 
         // 7 - Activez le VertexBufferObject contenant les faces (GL_ELEMENT_ARRAY_BUFFER)
+							   
+							   glAssert( glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexBufferObjects_[VBO_VERTICES]) );
         // 8 - Remplir le VertexBufferObject contenant les faces
-
+				 glAssert( glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3*nbTriangles_ * sizeof(int), &(triangles[0]),GL_STATIC_DRAW);
         // Fin du code à écrire
+				glAssert(glBindVertexArray(0) );
+																											   
 
 
     }
@@ -60,9 +97,14 @@ namespace rendersystem {
 
         // Début du code à écrire
         // 1 - Activer le VAO
+		
+		glAssert( glBindVertexArray(VertexArrayObject_));
+
 
         // 2 - Dessiner les triangles. Les sommets des triangles étant indexés et non consécutifs (sauf cas très particulier)
         // on utilisera la fonction glDrawElements(...)
+
+		glAssert( glDrawElements(GL_TRIANGLES, 3*nbTriangles_, GL_UNSIGNED_INT, NULL) );
 
         // Fin du code à écrire
 
