4c3bf3299dda9bf55fc18693398c2069e072318a
JackDanny <jackdanny@jackdanny-desktop.(none)>
2014-02-19 15:22:16 +0100

    continuation TP IAA

--- IAA/TP4/kppv.py
+++ IAA/TP4/kppv.py
@@ -38,10 +38,45 @@ def affichage_kppv(liste_donnees_classes,point_a_classer,K):
 		
 	# Affichage du point a classer
 	# ...
-		
+        print("point a classer " +str(point_a_classer))
+        plot(point_a_classer[0],point_a_classer[1],'Dk')
+
 	# Affichage des segments sur les K points les plus proches
 	# ...
-		
+
+        #liste des distances du point à classer à chaque points de liste_donnees_classes
+        distances_class = []
+
+        for i  in range(nb_classes):
+                #on stock aussi le num de la classe i et sa position dans cette classe j
+                for j in range(len(liste_donnees_classes[i])):
+                        distances_class.append( (norm(  (array(point_a_classer)) - (array(liste_donnees_classes[i][j]))  ),i,j ))
+
+        #on trie la liste par distances croissantes
+        distances_class.sort(comp)
+        #print("distances_class apres sort" + str(distances_class))
+
+        #on va relier les K plus proches voisins
+        for i in range(K):
+            #on recupere l'indice de la classe à laquelle appartient le ieme point le plus prêt
+            i1=distances_class[i][1]
+            #... ainsi que sa position dans la sous-liste
+            i2=distances_class[i][2]
+
+
+            #on recupère donc l'abcisse du point de la i1 ieme classe à la position i2
+            x=liste_donnees_classes[i1][i2][0]
+            #print("dc 1" +str(liste_donnees_classes[i1][i2][1]))
+
+            #et l'ordonnée du point de la i1 ieme classe à la position i2
+            y=liste_donnees_classes[i1][i2][1]
+
+            #on relie le point à classer au point de coordonées x y
+            plot([point_a_classer[0],x],[point_a_classer[1],y],'r-')
+
+        #on normalise les axes pour mieux "voir" les distances
+        axis('equal')
+
 	show()
 	hold(False)
 	
@@ -65,44 +100,81 @@ def decision_kppv(liste_points_classes,point_a_classer,K):
 	# ...
 	
 	distances_class = []
-	for i  in range(len(liste_points_classes)):
-		
+        for i  in range(nb_classes):
+                #pour chaque distance on va aussi stocker la classe à laquelle il appartient
 		for j in range(len(liste_points_classes[i])):
-			distances_class.append( (norm(array(point_a_classer)-array(liste_points_classes[i][j])),i ))
+                        distances_class.append( (norm(array(point_a_classer)-array(liste_points_classes[i][j])),i ))
 		
 	#print("distances_class avant sort" + str(distances_class))
-	distances_class.sort(comp)
-	#print("distances_class apres sort" + str(distances_class))
-
+        distances_class.sort(comp)
+        #print("distances_class apres sort" + str(distances_class))
+        #print("distances: "+str(distances_class))
 	
 	
 	# recherche des points les plus proches
 	# ...
 	
+        #on compte le nombre d'occurence des classes auxquelles appartiennent les points
 	nbOccClass=[0]
-	for i in range(K):
+        for i in range(len(liste_points_classes)-1):
 		nbOccClass.append(0)
 	
+
 	for i in range(K):
-		c=distances_class[i][1]
+                #le ieme point le plus proche appatient à la classe c
+                c=distances_class[i][1]
+                #on incrémente donc nbOccClass de 1
 		nbOccClass[c]=nbOccClass[c]+1
 	
-	# décision sur les classes
+        print("nbOccClasse = "+str(nbOccClass))
+        # décision sur les classes
 	# ...
-	classe_la_plus_proche=argmax(nbOccClass)
+
+        #on prend l'indice de l'élément de plus grande valeur de nbOccClass
+        classe_la_plus_proche=argmax(nbOccClass)
+
+        #liste des classes ayant max(nbOccClass) occurences
+        #ceci sert à savoir si on a un cas d'indécision
+        classArgmax=[]
+
+        for i in range(len(nbOccClass)):
+            if(nbOccClass[i] == max(nbOccClass)):
+                classArgmax.append(i)
+
+        #print("classArgmax" + str(classArgmax))
+
+        #si la liste n'a pas 1 éléments, elle en a plusieurs donc on est dans un cas d'indécision
+        if(nbOccClass.count(nbOccClass[classe_la_plus_proche]) != 1):
+            #prendPasDecision(nbOccClass,classArgmax)
+            classe_la_plus_proche = prendDecisionRandom(classArgmax)
+
+
+
 	
 	# classe_la_plus_proche = ...
-	
-	for i in range(len(nbOccClass)):
-		if ( (i!= classe_la_plus_proche) and (nbOccClass[i] == nbOccClass[classe_la_plus_proche]) ):
-		    print("erreur, indécision!!!")
-		    
-		
-    
+
 	return classe_la_plus_proche
+
+
+def prendPasDecision(nbOccClass,classArgmax):
+    print("nbOccClass = " + str(nbOccClass))
+
+    print("les classes qui portent soucis sont:" +str(array(classArgmax)+1))
+
+    raise Exception("indecision")
+    pass
+
+def prendDecisionRandom(listeClasse):
+    r=randint(0,len(listeClasse))
+    return listeClasse[r]
+
+
+
 
 # Programme principal : préparation points représentatifs et
 #                       test sur 3 points
+
+close('all')
 
 donnees_classe1 = [[3.6484028273127254, 0.073429844289042881], [2.330634259457685, 0.21748520551489497], [2.644948143301538, 0.20411304520644272], [2.7753121336336251, 0.18681685779827709], [2.1983916903402347, 0.19693911318338075], [2.836900928011457, 0.16884540288894506], [2.6235769902396133, 0.14047193438597244], [2.7157154911382348, 0.18092519035140286], [2.6662657025141492, 0.17384029751490698], [2.7900235283901487, 0.17124467162767718], [2.3025641883523864, 0.19080233853944761], [2.50402381354598, 0.26502653237173324], [2.4698500051963022, 0.23443828139261397], [2.5051902610502612, 0.23067614484914289], [2.4684409160318621, 0.21863502590393655], [3.0295456522668971, 0.086190046340667389], [2.4470385350120138, 0.21074952985853601], [2.5841314160351514, 0.1399509161051439], [2.9340821512886901, 0.13392607949486274]]
 donnees_classe2 = [[4.0958394694071085, 0.62090992691837554], [3.0515154047252695, 0.53512517722257091], [2.9491682603712408, 0.44028070952077253], [3.2829314223632156, 0.49129132399409814], [3.2997815280896532, 0.46202148420611983], [3.1994813507553297, 0.51730303394481048], [3.2739397765597413, 0.42456810719843918], [3.0094501931685627, 0.53307647477381548], [3.1121716887339406, 0.61264659221630813], [3.1567904507066951, 0.56034384641028545], [3.1666032722891706, 0.47572772737933283], [2.8978523007369641, 0.39472962675440465], [3.3719622811831127, 0.57256856612568585], [3.0025688112273086, 0.5581421242147987], [3.2243758188595679, 0.52064997498366095], [3.0110164875805565, 0.43111214123321362], [3.1485374067025722, 0.64330361652352819], [3.2302342511064359, 0.62838600861174609], [3.4725953755453878, 0.43200027260585433]]
@@ -114,29 +186,40 @@ donnee_test_classe1 = [3.0299364237682695, 0.069933489299203899]
 donnee_test_classe2 = [3.3156657804150163, 0.67327144428452557]
 donnee_test_classe3 = [4.0484484569058861, 0.24216712711692681]
 
-K=4
+K=5
 
 print "Decision par K-PPV, avec K = ",int(K)
 
 decision1 = decision_kppv(liste_donnees_classes,donnee_test_classe1,K)
-print "La donnee de classe 1 a ete reconnue comme une donnee de classe ",int(decision1)
+print "La donnee de classe 1 a ete reconnue comme une donnee de classe ",int(decision1+1)
 figure()
 affichage_kppv(liste_donnees_classes,donnee_test_classe1,K)
 
 decision2 = decision_kppv(liste_donnees_classes,donnee_test_classe2,K)
-print "La donnee de classe 2 a ete reconnue comme une donnee de classe ",int(decision2)
+print "La donnee de classe 2 a ete reconnue comme une donnee de classe ",int(decision2+1)
 figure()
 affichage_kppv(liste_donnees_classes,donnee_test_classe2,K)
 
 decision3 = decision_kppv(liste_donnees_classes,donnee_test_classe3,K)
-print "La donnee de classe 3 a ete reconnue comme une donnee de classe ",int(decision3)
+print "La donnee de classe 3 a ete reconnue comme une donnee de classe ",int(decision3+1)
 figure()
 affichage_kppv(liste_donnees_classes,donnee_test_classe3,K)
 
+'''
 print "Cas d'indécision (K=5)"
 donnee_test_indecidable = [1.65, 1.02]
 decision = decision_kppv(liste_donnees_classes,donnee_test_indecidable,5)
 print "La donnee a ete reconnue comme une donnee de classe ",int(decision)," Normalement : indecidable."
+figure()
 affichage_kppv(liste_donnees_classes,donnee_test_indecidable,K)
+'''
+
+#traitement du cas d'égalité
 
 
+
+'''
+
+
+
+'''
